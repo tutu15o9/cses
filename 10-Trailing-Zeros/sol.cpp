20f925437abaa5c86c9ad39cb9cd214a83c10116
tutu15o9 <rahultuteja1509@gmail.com>
2021-10-15 22:07:50 +0530

    :ok_hand: 10-Trailing-Zeros

---
+++ 10-Trailing-Zeros/sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout << #x << "=" << x << endl
+#define ll long long
+void solve()
+{
+	int n,sol = 0;
+	cin >> n;
+
+	while(n > 0){
+		sol += n/5;
+		n /= 5;
+	}
+	cout << sol<< endl;
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t = 1;
+	// cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
---
+++ 10-Trailing-Zeros/sol.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class sol{
+    // Comparator<> compare = new Comparator<>() {
+    //     @Override
+    //     public int compare( a, b) {
+    //         return;
+    //     }
+    // };
+    public static void  solve(){
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt(),sol = 0;
+
+        while(n > 0){
+            sol += n/5;
+            n /= 5;
+        }
+        System.out.println(sol);
+    }
+    public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+		int t=1;
+        // t = sc.nextInt();
+        while(t>0){
+            solve();
+            t--;
+        }
+    }
+}
